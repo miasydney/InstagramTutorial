5f824d623170507c4583eb7c6d69d851f0452d41
mia <miabenn74@gmail.com>
2024-07-01 22:00:37 +1000

    Tab Bar Setup

---
+++ InstagramTutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 1/7/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
