1671816ddc6a29ffa928d39c3619a9e78e9c4aa5
mia <miabenn74@gmail.com>
2024-07-01 23:09:12 +1000

    Add SearchView

---
+++ InstagramTutorial/Core/Search/SearchView.swift
@@ -0,0 +1,51 @@
+//
+//  SearchView.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 1/7/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0 ... 13, id: \.self) { user in
+                        HStack {
+                            Image("profilephoto")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("batman")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Bruce Wayne")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
