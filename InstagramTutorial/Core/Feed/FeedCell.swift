1c9d87070834ea0565ce2433661047cd818a7981
mia <miabenn74@gmail.com>
2024-07-01 22:51:01 +1000

    Add FeedCell and FeedView

---
+++ InstagramTutorial/Core/Feed/FeedCell.swift
@@ -0,0 +1,98 @@
+//
+//  FeedCell.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 1/7/2024.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // image + username
+            HStack {
+                Image("profilephoto")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 36, height: 26)
+                    .clipShape(Circle())
+                
+                Text("batman")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            
+            // post image
+            
+            Image("newyork-image")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // action buttons
+            HStack(spacing: 16) {
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Comment on post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
+            
+            // likes label
+            
+            Text("23 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            
+            // caption label
+            
+            HStack {
+                Text("batman ").fontWeight(.semibold) +
+                Text("This is a test caption for now")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            Text("6h ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,42 @@
+//
+//  FeedView.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 1/7/2024.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("instagram_logo_gradient")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 32)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
