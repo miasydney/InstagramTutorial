ebb3d2c2e24f81d953b2fb7a4bb118dbfcfc179a
mia <miabenn74@gmail.com>
2024-07-03 11:49:23 +1000

    Refactor posts view by adding PostGridView and ProfileHeaderView to display in ProfileView and CurrentUserProfileView.

---
+++ InstagramTutorial/Core/Components/PostGridView.swift
@@ -0,0 +1,37 @@
+//
+//  PostGridView.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 3/7/2024.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+    
+    var posts: [Post]
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    private let imageDimension: CGFloat = (UIScreen.main.bounds.width / 3) - 1
+    
+    var body: some View {
+        LazyVGrid(columns: gridItems, spacing: 2) {
+            ForEach(posts) { post in
+                Image(post.imageUrl)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: imageDimension, height: imageDimension)
+                    .clipped()
+            }
+        }
+    }
+}
+
+#Preview {
+    PostGridView(posts: Post.MOCK_POSTS)
+}
--- InstagramTutorial/Core/Profile/CurrentUserProfileView.swift
+++ InstagramTutorial/Core/Profile/CurrentUserProfileView.swift
@@ -9,75 +9,26 @@ import SwiftUI
 
 struct CurrentUserProfileView: View {
     
+    let user: User
+    
     private let gridItems: [GridItem] = [
         .init(.flexible(), spacing: 1),
         .init(.flexible(), spacing: 1),
         .init(.flexible(), spacing: 1)
     ]
     
+    var posts: [Post] {
+        return Post.MOCK_POSTS.filter({$0.user?.username == user.username})
+    }
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 // header
-                VStack(spacing: 10) {
-                    // pic and stats
-                    HStack {
-                        Image("profilephoto")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                                     
-                        Spacer()
-                        
-                        HStack(spacing: 8) {
-                            UserStatView(value: 3, title: "Posts")
-                            
-                            UserStatView(value: 332, title: "Followers")
-                            
-                            UserStatView(value: 343, title: "Following")
-                        }
-                    }
-                    .padding(.horizontal)
-                    
-                    // name and bio
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Chadwick Boseman")
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                        
-                        Text("Wakanda")
-                            .font(.footnote)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    
-                    // action button
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("Edit Profile")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(width: 360, height: 32)
-                            .foregroundColor(.black)
-                            .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 1))
-                    }
-
-                    Divider()
-                    
-                }
+                ProfileHeaderView(user: user)
                 
                 // post grid view
-                
-                LazyVGrid(columns: gridItems, spacing: 2) {
-                    ForEach(0 ... 15, id: \.self) { index in
-                        Image("newyork-image")
-                            .resizable()
-                            .scaledToFill()
-                    }
-                }
+                PostGridView(posts: posts)
                 
             }
             .navigationTitle("Profile")
@@ -97,5 +48,5 @@ struct CurrentUserProfileView: View {
 }
 
 #Preview {
-    CurrentUserProfileView()
+    CurrentUserProfileView(user: User.MOCK_USERS[0])
 }
