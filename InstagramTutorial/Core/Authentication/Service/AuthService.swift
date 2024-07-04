e36ecd20b03c937683f58611a12838ab67311596
mia <miabenn74@gmail.com>
2024-07-05 01:10:15 +1000

    Upload User Data functionality

--- InstagramTutorial/Core/Authentication/Service/AuthService.swift
+++ InstagramTutorial/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 // contains all functions related to authenticating user and communicating with API
 
@@ -36,6 +38,9 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            print("DEBUG: Did create user..")
+            await uploadUserData(uid: result.user.uid, username: username, email: email)   // upload information about user to firebase
+            print("DEBUG: Did upload user data...")
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
@@ -48,6 +53,12 @@ class AuthService {
     func signOut() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
     
 }
