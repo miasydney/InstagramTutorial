158af11f4df54d00ec32a987789711084fbe104b
mia <miabenn74@gmail.com>
2024-07-11 14:00:02 +1000

    Uploading a Post

---
+++ InstagramTutorial/Core/UploadPost/ViewModel/UploadPostViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  UploadPostViewModel.swift
+//  InstagramTutorial
+//
+//  Created by Mia on 3/7/2024.
+//
+
+import Foundation
+import PhotosUI
+import SwiftUI
+import Firebase
+
+@MainActor
+class UploadPostViewModel: ObservableObject {
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet { Task { await loadImage(fromItem: selectedImage) } }
+    }
+    @Published var postImage: Image?
+    private var uiImage: UIImage?
+    
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return } // get the image data from the photo picker item
+        guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
+        self.postImage = Image(uiImage: uiImage)
+    }
+    
+    func uploadPost(caption: String) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return } // make sure someone is logged in and we have their uid
+        guard let uiImage = uiImge else { return } // make sure we have an image before we upload a post
+        
+        let postRef = Firestore.firestore().collection("posts").document() // firebase generates a document identifier
+        guard let imageUrl = try await ImageUploader.uploadImage(image: uiImage) else { return }         // upload image
+        let post = Post(id: postRef.documentID, ownerUid: uid, caption: caption, likes: 0, imageUrl: imageUrl, timestamp: Timestamp())         // upload post
+        guard let encodedPost = try? Firestore.Encoder().encode(post) else { return }
+        
+        try await postRef.setData(encodedPost)
+    }
+}
--- InstagramTutorial/Model/Post.swift
+++ InstagramTutorial/Model/Post.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 // represents a users post
 struct Post: Identifiable, Hashable, Codable {
@@ -14,17 +15,17 @@ struct Post: Identifiable, Hashable, Codable {
     let caption: String
     var likes: Int
     let imageUrl: String
-    let timestamp: Date
+    let timestamp: Timestamp // custom firebase timestamp object
     var user: User?
 }
 
 // mock posts
 extension Post {
     static var MOCK_POSTS: [Post] = [
-        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "This is some test caption for now", likes: 120, imageUrl: "newyork-image", timestamp: Date(), user: User.MOCK_USERS[0]),
-        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Exploring the city vibes 🌆", likes: 85, imageUrl: "tenniscourt", timestamp: Date(timeIntervalSinceNow: -3600), user: User.MOCK_USERS[1]),
-        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Sunset views from the beach 🌅", likes: 200, imageUrl: "beach", timestamp: Date(timeIntervalSinceNow: -7200), user: User.MOCK_USERS[2]),
-        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Coffee and books - perfect combo! ☕️📚", likes: 150, imageUrl: "coffee", timestamp: Date(timeIntervalSinceNow: -10800), user: User.MOCK_USERS[0]),
-        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Hiking adventures in the mountains ⛰️", likes: 180, imageUrl: "nature", timestamp: Date(timeIntervalSinceNow: -14400), user: User.MOCK_USERS[1])
+        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "This is some test caption for now", likes: 120, imageUrl: "newyork-image", timestamp: Timestamp(), user: User.MOCK_USERS[0]),
+        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Exploring the city vibes 🌆", likes: 85, imageUrl: "tenniscourt", timestamp: Timestamp(), user: User.MOCK_USERS[1]),
+        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Sunset views from the beach 🌅", likes: 200, imageUrl: "beach", timestamp: Timestamp(), user: User.MOCK_USERS[2]),
+        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Coffee and books - perfect combo! ☕️📚", likes: 150, imageUrl: "coffee", timestamp: Timestamp(), user: User.MOCK_USERS[0]),
+        .init(id: NSUUID().uuidString, ownerUid: NSUUID().uuidString, caption: "Hiking adventures in the mountains ⛰️", likes: 180, imageUrl: "nature", timestamp: Timestamp(), user: User.MOCK_USERS[1])
     ]
 }
